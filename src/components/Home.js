655f8470427d5264a46d49cb384cb120cc0a518d
olympia <owojcik7@gmail.com>
2019-10-30 19:10:20 -0400

    Set-up the home route

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,38 @@
 import React, { Component } from "react"
+import TeamLogo from "./TeamLogo"
+import { Link } from "react-router-dom"
+import { getTeamNames } from "../api"
 
 export default class Home extends Component {
+    state = {
+        teamNames: []
+    }
+    //fetch teams using getTeamNames, add to state which will cause a re-render
+
+    componentDidMount() {
+        getTeamNames()
+            .then((teamNames) => this.setState(() => ({
+                teamNames
+            })))
+            .catch((error) => console.log('error'))
+    }
     render() {
+        const { teamNames } = this.state
         return (
             <div className="container">
-                Home
+                <h1 className="large-header">
+                    Hash History Basketball League
+                </h1>
+                <h3 className="header text-center">
+                    Select a team
+                </h3>
+                <div className="home-grid">
+                    {teamNames.map((id) => (
+                        <Link key={id} to={`/${id}`}>
+                            <TeamLogo id={id} width='125px'></TeamLogo>
+                        </Link>
+                    ))}
+                </div>
             </div>
         )
     }
--- src/dummy-data.js
+++ src/dummy-data.js
@@ -1,5 +1,6 @@
 import slug from 'slug'
 
+//array of player objects w/ position, teamId, number, avatar, & some states
 const players = [
     { name: 'Tyler McGinnis', position: 'Point Guard', teamId: 'bulls', number: 33, avatar: 'https://avatars.io/twitter/tylermcginnis', rpg: 3, spg: 4, apg: 8, ppg: 12 },
     { name: 'Jared Palmer', position: 'Shooting Guard', teamId: 'bulls', number: 15, avatar: 'https://avatars.io/twitter/jaredpalmer', rpg: 5, spg: 2, apg: 4, ppg: 15 },
@@ -28,6 +29,7 @@ const players = [
     { name: 'Ken Wheeler', position: 'Center', teamId: 'koalas', number: 69, avatar: 'https://avatars.io/twitter/ken_wheeler', rpg: 10, spg: 1, apg: 1, ppg: 10 },
 ]
 
+//teams an object w/ keys of team names and value is an object of info about that team w/ keys id, name, win, losses, coach, maanger, etc. championships is an array of dates
 const teams = {
     bulls: {
         id: 'bulls',
@@ -86,8 +88,10 @@ const teams = {
     },
 }
 
+//string
 const articleText = `Quae phasellus? Ullamcorper nunc ad eu tempus nostra, etiam massa, perferendis suscipit interdum, voluptates, tempora leo, nec, dignissim, laudantium diam tortor provident, eum erat, optio lorem rem class volutpat hac, eros per vivamus quisque, mauris incididunt! Quam ornare! Nam posuere metus? Laudantium esse provident nibh iure dolor modi! Voluptatibus porttitor magni nisl? Voluptatem mi, pariatur et nascetur! Hymenaeos maecenas viverra! Dolorum distinctio suspendisse, imperdiet! Aliquip quisquam quod, lacus beatae auctor. Ipsa fringilla! Minim molestie quo? Adipiscing, dui, fermentum excepteur luctus, conubia facilisi voluptatibus unde alias est possimus quibusdam, totam, quos? Recusandae egestas corporis, similique potenti facilisi? Reprehenderit massa voluptatum, asperiores. Eius libero sociosqu cras mollis. Maxime penatibus nascetur praesent eius explicabo, molestie? Deserunt inventore asperiores incididunt, venenatis recusandae potenti doloribus assumenda odit, tempora voluptatem dignissimos suscipit ridiculus, dicta quibusdam consequatur mauris, perspiciatis repellendus aute pede sapien. Blandit fringilla quisque, faucibus! Accusantium quis. Magna eros facere sollicitudin ornare sapiente placeat malesuada tempus hac? Provident inventore dignissim habitant. Per aliquet veritatis. Quasi mattis repellendus dapibus mi! Suscipit labore vulputate quae impedit hymenaeos! Sodales pede optio perferendis? Architecto iusto nullam condimentum ac expedita! Cursus. Natoque eu minim, ut ullam venenatis, varius dignissimos per! Scelerisque. Platea ut nec convallis mauris. Optio laudantium tristique cursus. Quibusdam, vero quod culpa magnam magna consectetur maiores quisque blandit sunt ab viverra commodo hic aliquam morbi cumque! Officia netus blandit laboriosam tortor ex lectus, fames habitant similique, ex convallis, blandit neque. Quidem dignissim ridiculus, ipsa, vero eum non class nostrum labore? Class dis nesciunt, pellentesque iure facilisi, error placerat aliquet semper? Commodo ultrices? Id habitant congue ratione nascetur phasellus itaque pariatur dignissim, similique pariatur, fugiat! Tincidunt dolore ullamco quo, beatae deserunt nesciunt posuere deserunt scelerisque proin, dignissimos, nesciunt enim! Ut, inventore quibusdam dicta? Habitasse cupidatat quaerat porttitor neque optio! Gravida? Minima! Fugit? Eius deserunt ullamcorper? Iure eos, tincidunt? Exercitation. Fugiat reiciendis quos mus, fugiat, consequat? Aenean beatae aliquam lectus tempor habitant quibusdam ipsam aliquet venenatis. Sapien ullam venenatis cupiditate nobis cursus ratione litora? Mollitia odit tenetur scelerisque, excepteur facilisi? Porro maecenas molestiae per, quasi, litora, tincidunt numquam mattis laborum! Lacus urna ligula. Natus litora. Viverra odit exercitation ad convallis sociis minima. Tincidunt ullamco! Sagittis qui, inventore ratione fuga eaque, beatae eu integer, maiores, eius similique sodales interdum mus distinctio vitae labore est earum sequi, dictum? Condimentum veniam! Atque minim! Voluptatibus donec dolorem temporibus. Metus ducimus feugiat tempore! Optio aliqua lacinia morbi pulvinar mollis laboriosam fames, ante eveniet harum, facere.`
 
+//array of objects w keys title & date
 const articleInfo = [
     { title: 'close out big game', date: new Date(2018, 0, 1) },
     { title: "fall into late slump", date: new Date(2017, 11, 29) },
@@ -105,6 +109,7 @@ const articleInfo = [
     { title: "have eye on the prize", date: new Date(2017, 6, 6) },
     { title: "are all washed up", date: new Date(2017, 5, 4) },
 ]
+
 
 function generateTitle(teamId, index) {
     return teamId[0].toUpperCase() + teamId.slice(1) + ' ' + articleInfo[index].title
