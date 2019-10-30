b69addee5d21a2bd9386954d303d01e96dda357b
olympia <owojcik7@gmail.com>
2019-10-30 15:36:55 -0400

    Set-up initial Home, Players, and Teams routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import Home from "./Home"
+import Players from "./Players"
+import Teams from "./Teams"
+import Nav from "./Nav"
 
 function App() {
   return (
     <div>
-      React Router Course
+      <Router>
+        <div>
+          <Nav />
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/players" component={Players}></Route>
+          <Route path="/teams" component={Teams}></Route>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+
+export default function NavBar() {
+    return (
+        <div className="container navbar">
+            <Link to="/">Home</Link>
+            <nav className="nav-links">
+                <Link to="/players">Players</Link>
+                <Link to="/teams">Teams</Link>
+            </nav>
+
+        </div>
+    )
+}
