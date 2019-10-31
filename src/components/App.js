5c0247f453261128a142837e45d44dcd75f88934
olympia <owojcik7@gmail.com>
2019-10-31 13:53:50 -0400

    Set up team's page

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Home from "./Home"
 import Players from "./Players"
 import Teams from "./Teams"
 import Nav from "./Nav"
+import TeamPage from "./TeamPage"
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
             <Route path="/" exact component={Home}></Route>
             <Route path="/players" component={Players}></Route>
             <Route path="/teams" component={Teams}></Route>
-            {/* Route w/o path will always match but will only match if none match before it when using switch*/}
+            {/* Route w/o path will always match but will only match if none match before it when using switch*  - :team is ambiguous so move to bottom*/}
+            <Route path="/:teamId" component={TeamPage}></Route>
             <Route render={() => <h1 className="text-center">404 - This endpoint does not exist</h1>}></Route>
           </Switch>
         </div>
